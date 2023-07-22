060e6cc99f1d6923986125d1a406de6934e441e0
ChrisEng0212 <cjx02121981@gmail.com>
2023-07-22 15:57:38 +0800

    docker-worker

---
+++ meta.py
@@ -0,0 +1,36 @@
+
+import redis
+from pybit import inverse_perpetual, usdt_perpetual
+import os
+
+try:
+    import config
+    API_KEY = config.API_KEY
+    API_SECRET = config.API_SECRET
+    REDIS_PASS = config.REDIS_PASS
+    REDIS_IP = config.REDIS_IP
+    LOCAL = True
+except Exception as e:
+    print('GET ENV', e)
+    API_KEY = os.getenv('API_KEY')
+    API_SECRET = os.getenv('API_SECRET')
+    REDIS_PASS = os.getenv('REDIS_PASS')
+    REDIS_IP = os.getenv('REDIS_IP')
+    LOCAL = False
+
+
+r = redis.Redis(
+    host=REDIS_IP,
+    port=6379,
+    password=REDIS_PASS,
+    decode_responses=True
+    )
+
+session = inverse_perpetual.HTTP(
+    endpoint='https://api.bybit.com',
+    api_key=API_KEY,
+    api_secret=API_SECRET
+)
+
+print('REDIS', r)
+print('API', session)
